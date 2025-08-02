f373ba4c25da5ebb39c5b65abf7de051af69af09
Papp Dávid <pappdavidlevele@gmail.com>
2025-08-02 22:37:09 +0200

    successfully tested tailwind and vue chart js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
+import BarChart from './components/BarChart.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p class="text-xl underline" >tailwind test</p>
+   
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <BarChart />
 </template>
 
 <style scoped>
